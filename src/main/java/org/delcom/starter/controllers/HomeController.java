272c07bfbd4e2000b7126592a5259386b991fe8a
Giva Violen <givapardede0055@gmail.com>
2025-11-01 10:38:36 +0700

    Penambahan method sayHello() dan  pengujiannya

--- src/main/java/org/delcom/starter/controllers/HomeController.java
+++ src/main/java/org/delcom/starter/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.delcom.starter.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,8 @@ public class HomeController {
     public String hello() {
         return "Hay Abdullah, selamat datang di pengembangan aplikasi dengan Spring Boot!";
     }
-
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
